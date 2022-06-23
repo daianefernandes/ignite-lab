6d6f4f46da42cd8cb3f1f0906d87733d57aa5163
daianefernandes <daianedsdaia@gmail.com>
2022-06-22 21:11:29 -0300

    criando projeto e componentes iniciais

---
+++ src/components/Class.tsx
@@ -0,0 +1,5 @@
+export function Class() {
+    return (
+        <h1>Class</h1>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      colors: {
+        purple: {
+          300: '#AF07D0',
+          500: '#9A04BC',
+          700: '#8600A9',
+        },
+        blue: {
+          500: '#81D8F7',
+        },
+        orange: {
+          500: '#FBA94C',
+        },
+        red: {
+          500: '#F75A68',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          500: '#323238',
+          600: '#29292E',
+          700: '#121214',
+          900: '#09090A'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
