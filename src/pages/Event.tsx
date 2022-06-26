4f334e10f40f764ee3ab405016a3c86ddd45f3e0
daianefernandes <daianedsdaia@gmail.com>
2022-06-26 19:21:51 -0300

    adicionando novas implementacoes

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,7 +1,20 @@
 import { Header } from "../components/Header";
+import { Video } from "../components/Video";
+import { Sidebar } from "../components/Sidebar";
+import { useParams } from "react-router";
 
 export function Event() {
-    return (
-        <Header />
-    )
+  const { slug } = useParams<{ slug: string }>();
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        { slug ? <Video /> 
+          : <div className="flex-1" />
+        }
+        <Sidebar />
+      </main>
+    </div>
+  );
 }
